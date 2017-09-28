0d830fdc6720cba274e02a1d726e57a062076340
Richard Seroter <richardseroter@Richards-MacBook-Pro-2.local>
2017-09-28 15:08:25 -0700

    Refactored and added custom actuator
    Added memory, created controller class, introduced custom actuator
    counter

--- hello-world/src/main/java/seroter/demo/HelloWorldApplication.java
+++ hello-world/src/main/java/seroter/demo/HelloWorldApplication.java
@@ -2,20 +2,12 @@ package seroter.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
 @SpringBootApplication
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
-	}
-	
-	@RequestMapping("/hello-service")
-	public String SayHello(@RequestParam String name) {
-		return "Hello, " + name;
 	}
 }
